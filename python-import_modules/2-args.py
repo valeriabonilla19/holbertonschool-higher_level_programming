3ad410151821da6233138b73aeacf5cd51a4aae3
valeriabonilla19 <valeriabonillarivera02@gmail.com>
2025-05-11 21:58:46 -0400

    task_2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    num_args = len(sys.argv) - 1  # Exclude the script name itself
+    if num_args == 0:
+        print("0 arguments.")
+    elif num_args == 1:
+        print("1 argument:")
+        print(f"1: {sys.argv[1]}")
+    else:
+        print(f"{num_args} arguments:")
+        for i in range(1, num_args + 1):
+            print(f"{i}: {sys.argv[i]}")
