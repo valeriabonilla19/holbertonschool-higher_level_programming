a8bbd24ff2c026c0ded91b2e5b2b03dcfeffe4d8
valeriabonilla19 <valeriabonillarivera02@gmail.com>
2025-05-11 22:13:00 -0400

    task_3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import sys
 
-total = 0
-for arg in sys.argv[1:]:
-    total += int(arg)
+if __name__ == "__main__":  # Ensures the code runs only when executed directly
+    total = 0
+    for arg in sys.argv[1:]:
+        total += int(arg)
 
-# Split the print statement for long results
-print(total)
+    print(total)
