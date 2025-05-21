eec6d727dc1fae5977773d8ab04606bec15145bd
valeriabonilla19 <valeriabonillarivera02@gmail.com>
2025-05-21 16:39:49 -0400

    task_5

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module that defines a function to count the number of keys in a dictionary.
+"""
+
+
+def number_keys(a_dictionary):
+    """
+    Returns the number of keys in a dictionary.
+
+    Args:
+        a_dictionary (dict): The dictionary to evaluate.
+
+    Returns:
+        int: Number of keys in the dictionary.
+    """
+    return len(a_dictionary)
