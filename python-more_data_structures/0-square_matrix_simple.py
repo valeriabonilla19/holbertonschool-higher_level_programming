a84bb5723545cc09a50c493037e2b59d397d67c9
valeriabonilla19 <valeriabonillarivera02@gmail.com>
2025-05-21 15:43:04 -0400

    task_0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -4,18 +4,15 @@ Module that defines a function to square all integers in a matrix.
 """
 
 
-from typing import List
-
-
-def square_matrix_simple(matrix: List[List[int]] = None) -> List[List[int]]:
+def square_matrix_simple(matrix=None):
     """
     Computes the square value of all integers of a matrix.
 
     Args:
-        matrix (List[List[int]]): 2D list of integers.
+        matrix (list of lists of int): 2D list of integers.
 
     Returns:
-        List[List[int]]: New matrix with squared values.
+        list of lists of int: New matrix with squared values.
     """
     if matrix is None:
         return []
