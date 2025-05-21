adce54bcf84d4a62f8b4f0c200fa87a631d851e3
valeriabonilla19 <valeriabonillarivera02@gmail.com>
2025-05-21 15:41:47 -0400

    task_0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,23 @@
-def square_matrix_simple(matrix=[]):
-    # Create a new matrix by squaring each element of the input matrix
+#!/usr/bin/python3
+"""
+Module that defines a function to square all integers in a matrix.
+"""
+
+
+from typing import List
+
+
+def square_matrix_simple(matrix: List[List[int]] = None) -> List[List[int]]:
+    """
+    Computes the square value of all integers of a matrix.
+
+    Args:
+        matrix (List[List[int]]): 2D list of integers.
+
+    Returns:
+        List[List[int]]: New matrix with squared values.
+    """
+    if matrix is None:
+        return []
+
     return [[element ** 2 for element in row] for row in matrix]
