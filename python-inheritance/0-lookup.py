0906665df67f8d957ba838b7442a27f6782b1b55
valeriabonilla19 <valeriabonillarivera02@gmail.com>
2025-06-01 21:42:00 -0400

    task_0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""Module that defines the `lookup` function."""
+
 def lookup(obj):
-    """Returns the list of available attributes and methods of an object"""
+    """Returns the list of available attributes and methods of an object."""
     return dir(obj)
