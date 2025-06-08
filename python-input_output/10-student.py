196d24bd8f7f00b4c197cd35be73e83695446d8e
valeriabonilla19 <valeriabonillarivera02@gmail.com>
2025-06-08 17:40:14 -0400

    task_10

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,3 +1,28 @@
+#!/usr/bin/python3
+"""
+Defines a Student class with public attributes and
+a to_json method that can filter attributes.
+"""
+
+
+class Student:
+    """
+    Student class with first_name, last_name, and age attributes.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student instance.
+
+        Args:
+            first_name (str): First name of the student.
+            last_name (str): Last name of the student.
+            age (int): Age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
     def to_json(self, attrs=None):
         """
         Returns the dictionary representation of the Student instance.
